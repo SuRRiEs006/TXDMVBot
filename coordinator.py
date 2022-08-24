584192d9d9de5dd935be23e02c3033997524dd55
SuRRiEs006 <43891244+SuRRiEs006@users.noreply.github.com>
2022-08-24 05:55:03 +0100

    not gotten a test with this code yet.

--- coordinator.py
+++ coordinator.py
@@ -1,17 +1,17 @@
 import DMVSlotFinder
 import threading
 
-testProfile = DMVSlotFinder.BookingProfile("surya venkata sai vamsi krishna","sathanapalli","12/11/2002","1111","75034","08/23/2002")
-outcome = testProfile.main()
 
+outcome = False
 
 
 while True:
-    #testProfile = DMVSlotFinder.BookingProfile("profilef","profilel","11/11/2001","1111","75034","10/03/2002")
-    testProfile = DMVSlotFinder.BookingProfile("surya venkata sai vamsi krishna","sathanapalli","12/11/2002","1111","75034","08/23/2002")
-    #threading.Thread(target=DMVSlotFinder.BookingProfile, args=("profilef","profilel","11/11/2001","1111","75034","10/03/2002"))
+
+    testProfile = DMVSlotFinder.BookingProfile("surna","satpalli","12/2002","11","734","08/2002")
     outcome = testProfile.main()
     print(outcome)
+    if outcome != False:
+        break
     
     
     
