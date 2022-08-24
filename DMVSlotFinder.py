19782b84474188e4c60e82500797d2faba345e92
SuRRiEs006 <43891244+SuRRiEs006@users.noreply.github.com>
2022-08-24 07:03:57 +0100

    Final stable update, not tested in market. Up to 3 retries

--- DMVSlotFinder.py
+++ DMVSlotFinder.py
@@ -17,14 +17,14 @@ class BookingProfile:
         self.idealDate = idealDate #MM/DD/YEAR
         self.dateBooked = None
         self.browser = None
-        self.newDate = None
+        self.justBookedDate = None
         
 
 
     def bookAvalible(self,index):
         if index != 0:
             self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[2]/div/table/tbody/tr["+str(index)+"]/td[4]/i").click()
-            time.sleep(4)
+            time.sleep(5)
         self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[3]/table/tbody/tr/td[2]/div/div[1]/div").click()
         time.sleep(3)
         self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[2]/table/tbody/tr/td[2]/div/div/div").click()
@@ -33,8 +33,9 @@ class BookingProfile:
         time.sleep(6)
         self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[4]/div/div[2]/button").click()
         time.sleep(6)
-        self.newDate = (self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div/div[2]/div[2]/div[2]/div[1]/div[4]/div[2]").text).split(" ")[0]
-     
+        self.justBookedDate = (self.browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div/div[2]/div[2]/div[2]/div[1]/div[4]/div[2]").text).split(" ")[0]
+        
+        return(True)
 
     def enterText(self,xpath,text):
         self.browser.find_element_by_xpath(xpath).click()
@@ -48,78 +49,80 @@ class BookingProfile:
         if (formattedBooked > formattedNew) and (formattedNew >= formattedEarliest):
             print("FOUNDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
             self.bookAvalible(index)
-            if self.newDate == self.idealDate:
+
+            if time.strptime(self.justBookedDate, "%m/%d/%Y") == time.strptime( self.idealDate, "%m/%d/%Y"):
                 self.noNeedToRun = True
             else:
                 self.noNeedToRun = False
+            return(True)
 
 
     def main(self):
-        try:
-            browser = webdriver.Chrome("chromedriver.exe")
-            self.browser = browser
-            browser.get("https://public.txdpsscheduler.com/")
-            time.sleep(4)
-            browser.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/button[1]").click()
-            time.sleep(4)
+        #try:
+        browser = webdriver.Chrome("chromedriver.exe")
+        self.browser = browser
+        browser.get("https://public.txdpsscheduler.com/")
+        time.sleep(4)
+        browser.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/button[1]").click()
+        time.sleep(4)
 
-            self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[2]/div/div[1]/div/input",self.applicantFirstName)
-            time.sleep(0.5)
+        self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[2]/div/div[1]/div/input",self.applicantFirstName)
+        time.sleep(0.5)
 
-            self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[3]/div/div[1]/div/input",self.applicantLastName)
-            time.sleep(0.5)
+        self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[3]/div/div[1]/div/input",self.applicantLastName)
+        time.sleep(0.5)
 
-            self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[4]/div/div[1]/div/input",self.DOB)
-            time.sleep(0.5)
+        self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[4]/div/div[1]/div/input",self.DOB)
+        time.sleep(0.5)
 
-            self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[5]/div/div[1]/div/input",self.lastFourSSN)
-            time.sleep(0.5)
-
-
-            browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[4]/button").click()
-
-            time.sleep(10)
-            browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/div[5]/div/button").click()
+        self.enterText("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[3]/div[5]/div/div[1]/div/input",self.lastFourSSN)
+        time.sleep(0.5)
 
 
+        browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/form/div[2]/div[4]/button").click()
+
+        time.sleep(10)
+        browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/div[5]/div/button").click()
 
 
 
-            time.sleep(3)
-
-            self.dateBooked = (browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/div[3]/div[2]/div[2]/div[1]/div[4]/div[2]").text).split(" ")[0]
-
-            print(self.dateBooked)
-
-            browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/div[5]/div[2]/div[2]/button").click()
-
-            time.sleep(2)
-            browser.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[1]/button").click()
 
 
-            time.sleep(3)
+        time.sleep(3)
 
-            browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[1]/div[4]/div[1]/div/div/div[1]/div[1]/input").click()
-            browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[1]/div[4]/div[1]/div/div/div[1]/div[1]/input").send_keys(self.postCode)
+        self.dateBooked = (browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/div[3]/div[2]/div[2]/div[1]/div[4]/div[2]").text).split(" ")[0]
 
-            earlierFoundBooked = False
-            while (earlierFoundBooked != True):
-                browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[2]/div/div[2]/button").click()
-                time.sleep(11)
+        print(self.dateBooked)
 
-                index = 1
-                dateAvalible = browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[1]/div/table/tbody/tr/td[3]").text
-                earlierFoundBooked = self.isEarlierDateFound(dateAvalible,0)
-                while (earlierFoundBooked != True) and (index <= 4):
-                    dateAvalible = browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[2]/div/table/tbody/tr["+str(index)+"]/td[3]").text
-                    earlierFoundBooked = self.isEarlierDateFound(dateAvalible,index)
-                    index += 1
-                time.sleep(random.randint(30,60))
-                if earlierFoundBooked != True :
-                    browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[5]/div/div[1]/button").click()
-                else:
-                    return(self.noNeedToRun)
-        except:
-            self.browser.save_screenshot(self.applicantFirstName + str(datetime.now().strftime("     %d-%m-%Y-%H-%M-%S")) + ".png")
-            return(None)
+        browser.find_element_by_xpath("/html/body/div[1]/div/main/div/div/section/div/main/div/section/div[2]/div/div/div[5]/div[2]/div[2]/button").click()
+
+        time.sleep(2)
+        browser.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/div[2]/div[2]/div[1]/button").click()
+
+
+        time.sleep(3)
+
+        browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[1]/div[4]/div[1]/div/div/div[1]/div[1]/input").click()
+        browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[1]/div[4]/div[1]/div/div/div[1]/div[1]/input").send_keys(self.postCode)
+
+        earlierFoundBooked = False
+        while (earlierFoundBooked != True):
+            browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/form/div/div[2]/div[2]/div/div[2]/button").click()
+            time.sleep(11)
+
+            index = 1
+            dateAvalible = browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[1]/div/table/tbody/tr/td[3]").text
+            earlierFoundBooked = self.isEarlierDateFound(dateAvalible,0)
+            while (earlierFoundBooked != True) and (index <= 4):
+                dateAvalible = browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[2]/div/table/tbody/tr["+str(index)+"]/td[3]").text
+                earlierFoundBooked = self.isEarlierDateFound(dateAvalible,index)
+                index += 1
+            time.sleep(random.randint(30,60))
+            if earlierFoundBooked != True :
+                browser.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/section/div/main/div/section/div[2]/div/div[5]/div/div[1]/button").click()
+            else:
+                return(self.noNeedToRun)
+        # except:
+        #     self.browser.save_screenshot(self.applicantFirstName + str(datetime.now().strftime("     %d-%m-%Y-%H-%M-%S")) + ".png")
+        #     return(None)
 
